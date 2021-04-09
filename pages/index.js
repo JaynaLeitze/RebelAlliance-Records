09df6acc73b57a5a227f1931fcb23ab95df0e118
Jayna Leitze <jayna.leitze@gmail.com>
2021-04-09 17:12:32 -0500

    get and display search data to dom

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,38 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ results }) {
+  console.log(results);
   return (
     <div className={styles.container}>
       <Head>
         <title>Rebel Alliance Database</title>
       </Head>
+      <h1>Rebel Alliance Records</h1>
+      {results.results.map((c) => (
+        <div>
+          <h3>Name: {c.name} </h3>
+          <p> Height: {c.height} </p>
+          <p> Mass: {c.mass} </p>
+          <p> Hair Color: {c.hair_color}</p>
+          <p> Birth Year: {c.birth_year}</p>
+          <p> Species: {c.species} </p>
+        </div>
+      ))}
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(
+    "https://swapi.dev/api/people/?search=luke&skywalker"
+  );
+  const data = await res.json();
+  console.log(data);
+
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
