c5968c54ac8bc97c0bd518d59549c60c75ea5f68
Jayna Leitze <jayna.leitze@gmail.com>
2021-04-10 10:41:01 -0500

    build character search input

--- components/CharacterSearch.js
+++ components/CharacterSearch.js
@@ -1,2 +1,15 @@
 import React, { useContext } from "react";
-import { SymptomContext } from "./Sym";
+import { SwapiContext } from "./SwapiProvider";
+import SearchField from "react-search-field";
+
+export const CharacterSearch = (props) => {
+  const { setTerms } = useContext(SwapiContext);
+
+  return (
+    <SearchField
+      placeholder="Explore the galaxy..."
+      onEnter={(keyEvent) => setTerms(keyEvent.target.value)}
+      onSearchClick={(keyEvent) => setTerms(keyEvent.target.value)}
+    />
+  );
+};
