39e32fa80dd5cdc89e04e0078a7408339f91e4d7
Jayna Leitze <jayna.leitze@gmail.com>
2021-04-13 11:53:24 -0500

    fetch and display homeworld data

--- components/Character.js
+++ components/Character.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 
 export const Character = ({ person }) => {
   const [film, setFilm] = useState([]);
+  const [homeworld, setHomeworld] = useState([]);
+  const [starships, setStarships] = useState([]);
 
   //   const getFilm = () => {
   //     return person.films.map((url) =>
@@ -12,21 +14,28 @@ export const Character = ({ person }) => {
   //   };
 
   const films = person.films;
-  console.log(films);
+  const planet = person.homeworld;
 
   useEffect(() => {
-    async function fetchAll() {
+    async function fetchFilms() {
       const data = await Promise.all(
         films.map((url) => fetch(url).then((r) => r.json()))
       ).then((a) => setFilm(a));
     }
-    fetchAll();
+
+    async function fetchPlanet() {
+      const data = await fetch(planet)
+        .then((r) => r.json())
+        .then((p) => setHomeworld(p));
+    }
+    fetchFilms();
+    fetchPlanet();
   }, []);
-  console.log(film);
+
   return (
     <div>
       <h3>Name: {person.name} </h3>
-      <p> Homeworld: {person.homeworld}</p>
+      <p> Homeworld: {homeworld.name}</p>
       <p> Height: {person.height} </p>
       <p> Mass: {person.mass} </p>
       <p> Hair Color: {person.hair_color}</p>
